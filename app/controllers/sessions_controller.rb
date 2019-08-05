697378fe2f36bb06bc1436a8d98f32079269ba7b
eapenzacharias <eapenzac@gmail.com>
2019-08-06 01:55:49 +0200

    Sign in auth added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 class SessionsController < ApplicationController
     def new
         
-    end  
+    end 
+    def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+          flash[:success] = 'Sign In sucessful'
+          sign_in user
+          redirect_to root_path        
+        else
+          flash.now[:error] = 'Invalid email/password combination'
+          render 'new'
+        end
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  root "static_pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:new,:create,:distroy]
   match '/signin', to: 'sessions#new', via: 'get'
