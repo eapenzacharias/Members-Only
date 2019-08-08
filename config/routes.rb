db630bd413d86521cebda8a693b14bfa3a8b9d53
eapenzacharias <eapenzac@gmail.com>
2019-08-08 23:52:30 +0200

    changes in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  get 'post/new'
-  get 'post/create'
-  get 'post/index'
-  get 'static_pages/home'
   root "post#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:new, :create, :destroy]
   match '/signin',  to: 'sessions#new', via: 'get'
   match '/signout', to: 'sessions#destroy', via: 'delete'
   resources :post, only: [:new, :create, :index]
-  match '/post/new',  to: 'post#new', via: 'post'
-  match '/post/create',  to: 'post#create', via: 'post'
 end
