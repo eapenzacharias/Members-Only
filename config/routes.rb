0ae51bcc34d9dc20807ae95cd58bb8d13037ef44
eapenzacharias <eapenzac@gmail.com>
2019-08-09 00:02:05 +0200

    sign up view created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "post#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :user, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  match '/signup',  to: 'user#new', via: 'get'
   match '/signin',  to: 'sessions#new', via: 'get'
   match '/signout', to: 'sessions#destroy', via: 'delete'
   resources :post, only: [:new, :create, :index]
