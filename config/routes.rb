755a8cb6261f3d7b00f3b83064d3867f5c4ff095
eapenzacharias <eapenzac@gmail.com>
2019-08-09 00:19:18 +0200

    Sign Up function added

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :user, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-  match '/signup',  to: 'user#new', via: 'get'
+  get '/signup', to: 'user#new'
+  post '/signup', to: 'user#create'
+
   match '/signin',  to: 'sessions#new', via: 'get'
-  match '/signout', to: 'sessions#destroy', via: 'delete'
+  delete '/signout', to: 'sessions#destroy'
   resources :post, only: [:new, :create, :index]
 end
