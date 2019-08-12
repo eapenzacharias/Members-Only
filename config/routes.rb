9f092304e87101259f238e83521ea795b27312a9
lilleonz <lilleonz@gmail.com>
2019-08-12 13:52:35 -0400

    changed requested changes from tse

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,51 @@
 Rails.application.routes.draw do
-  root "posts#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :user, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
-  get '/signup', to: 'user#new'
-  post '/signup', to: 'user#create'
+  root 'posts#index'
+  # resources :users, only: [:new, :create]
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
-  match '/signin',  to: 'sessions#new', via: 'get'
+  resources :sessions, only: [:new, :create, :destroy]
+  get '/signin', to: 'sessions#new'
   delete '/signout', to: 'sessions#destroy'
+
   resources :posts, only: [:new, :create, :index]
 end
+
+# This is why we left both for sessions for some reason with one or the other removed it all crashes
+
+=begin
+This is the error with resourcs :sessions commented out
+
+NameError in Sessions#new
+Showing /home/leonz/Documents/MicroVerse/rubyonrails/Members-Only/app/views/sessions/new.html.erb where line #2 raised:
+
+undefined local variable or method `sessions_path' for #<#<Class:0x00007fbc5e01a7a8>:0x00007fbc5e0184f8>
+Did you mean?  session
+Extracted source (around line #2):
+1
+2
+3
+4
+5
+6
+
+<h1>Sign In</h1>
+<%= form_for(:session, url: sessions_path) do |f| %>
+ 
+  <%= f.label :email, "Email:" %>
+  <%= f.email_field :email, placeholder: "user@mail.com" %><br/>
+  <%= f.label :password, "Password:" %>
+
+=end
+
+=begin
+This is the error when get and delte are commented out for sessions
+
+Routing Error
+No route matches [GET] "/signin"
+Rails.root: /home/leonz/Documents/MicroVerse/rubyonrails/Members-Only
+
+Application Trace | Framework Trace | Full Trace
+Routes
+
+=end
